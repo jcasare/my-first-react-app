60f9e96c31e2e695d8ecd39ed30fa3dccfcc35e9
jcasare <asarejerry16@gmail.com>
2023-02-28 16:18:02 +0000

    creating-components commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './styles.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+const Book = () => {
+  return (
+    <article className="single-book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51RljWNfhWL._SY346_.jpg"
+    alt="A secret in the keys"
+  />
+)
+const Author = () => <h4>Hope Holloway</h4>
+const Title = () => <h2>A secret in the keys</h2>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
