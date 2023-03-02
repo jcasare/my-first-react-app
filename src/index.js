91dd6869135f84210362c640adc5709933f85f3d
jcasare <asarejerry16@gmail.com>
2023-03-02 21:48:11 +0000

    iterating components over arrays and refactoring props

--- src/index.js
+++ src/index.js
@@ -3,55 +3,56 @@ import ReactDOM from 'react-dom/client'
 
 import './styles.css'
 
+const bookCollection = [
+  {
+    title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
+    imgLink:
+      'https://m.media-amazon.com/images/I/41Pw77onK7L._SX329_BO1,204,203,200_.jpg',
+    author: 'Ron DeSantis',
+    id: 1,
+  },
+  {
+    title: 'Never Give an Inch: Fighting for the America I Love',
+    imgLink:
+      'https://m.media-amazon.com/images/I/41OxPE1ZMbL._SX329_BO1,204,203,200_.jpg',
+    author: 'Mike Pompeo',
+    id: 2,
+  },
+  {
+    title: 'Start, Stay, or Leave: The Art of Decision Making',
+    imgLink:
+      'https://m.media-amazon.com/images/I/41qGiu+6bpL._SX331_BO1,204,203,200_.jpg',
+    author: 'Trey Gowdy',
+    id: 3,
+  },
+  {
+    title:
+      'Unified: How Our Unlikely Friendship Gives Us Hope for a Divided Country',
+    imgLink:
+      'https://m.media-amazon.com/images/I/41EiN-1Bb9L._SX331_BO1,204,203,200_.jpg',
+    author: 'Tim Scott',
+    id: 4,
+  },
+]
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        imgLink={collections.imgLinks[0]}
-        author={collections.authors[0]}
-        title={collections.titles[0]}
-      />
-      <Book
-        imgLink={collections.imgLinks[1]}
-        author={collections.authors[1]}
-        title={collections.titles[1]}
-      />
-      <Book
-        imgLink={collections.imgLinks[2]}
-        author={collections.authors[2]}
-        title={collections.titles[2]}
-      />
-      <Book
-        imgLink={collections.imgLinks[3]}
-        author={collections.authors[3]}
-        title={collections.titles[3]}
-      />
+      {bookCollection.map((book) => {
+        const { id } = book
+        return <Book {...book} key={id} />
+      })}
     </section>
   )
 }
-const collections = {
-  titles: [
-    "The Courage to Be Free: Florida's Blueprint for America's Revival",
-    'Never Give an Inch: Fighting for the America I Love',
-    'Start, Stay, or Leave: The Art of Decision Making',
-    'Unified: How Our Unlikely Friendship Gives Us Hope for a Divided Country',
-  ],
-  authors: ['Ron DeSantis', 'Mike Pompeo', 'Trey Gowdy', 'Tim Scott'],
 
-  imgLinks: [
-    'https://m.media-amazon.com/images/I/41Pw77onK7L._SX329_BO1,204,203,200_.jpg',
-    'https://m.media-amazon.com/images/I/41OxPE1ZMbL._SX329_BO1,204,203,200_.jpg',
-    'https://m.media-amazon.com/images/I/41qGiu+6bpL._SX331_BO1,204,203,200_.jpg',
-    'https://m.media-amazon.com/images/I/41EiN-1Bb9L._SX331_BO1,204,203,200_.jpg',
-  ],
-}
 const Book = (props) => {
-  console.log(props)
+  const { imgLink, title, author } = props
   return (
     <article className="single-book">
-      <img src={props.imgLink} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={imgLink} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
 }
